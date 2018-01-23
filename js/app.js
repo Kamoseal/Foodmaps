dc8e8e5d5ccf64104a2f1dbd8a82c2c611bd43e5
kamoseal <serna.karla@gmail.com>
2018-01-23 13:23:38 +0000

    Agregando archivo data y un par de funciones

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
+$(document).ready(function(){
+  //ocultar main
+    $("main").slideUp(4000);
+
+    var restaurante = data[0,"name"];
+    console.log("restaurante");
+
+
+//funcion de modales
+// $('#exampleModal').on('show.bs.modal', function (event) {
+//   var button = $(event.relatedTarget) // Button that triggered the modal
+//   var recipient = button.data('whatever') // Extract info from data-* attributes
+//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//   var modal = $(this)
+//   modal.find('.modal-title').text('New message to ' + recipient)
+//   modal.find('.modal-body input').val(recipient)
+// })
+
+
+
+
+
+
 })
---
+++ js/data.js
@@ -0,0 +1,43 @@
+var data = [{
+        'name': 'Hamburguesas Chuy',
+        'adress': 'Av. Miguel Hidalgo #24 Col. Sn Fco Tepojaco CI',
+        'photo': 'http://lorempixel.com/350/200/food/',
+        'horarios': '14 - 21'
+        'precios': '$60.00 a $100.00'
+    },
+    {
+        'name': 'La calle del hambre',
+        'adress': 'Cll Naucalpan Col. Cumbria CI.',
+        'photo': 'http://lorempixel.com/350/200/food/',
+        'horarios': '24 hrs'
+        'precios': '$60.00 a $180.00'
+    },
+    {
+        'name': 'La churrasqueria',
+        'adress': 'Av del sauce. Col. Zn Ind Tlanepantla.',
+        'photo': 'http://lorempixel.com/350/200/food/',
+        'horarios': '12 - 22'
+        'precios': '$260.00 a $480.00'
+    },
+    {
+        'name': 'Pozole Don Toño',
+        'adress': 'Av Chalma, Sn Marcos Power center CI',
+        'photo': 'http://lorempixel.com/350/200/food/',
+        'horarios': '11 - 22'
+        'precios': '$60.00 a $380.00'
+    },
+    {
+        'name': 'Las alitas',
+        'adress': 'Calzada Via vallejo, Col. La Raza CDMX',
+        'photo': 'http://lorempixel.com/350/200/food/',
+        'horarios': '14 - 24'
+        'precios': '$260.00 a $780.00'
+    },
+    {
+        'name': 'Cortes Brasileños',
+        'adress': 'Cll Toluca 11. Col Cumbria CI',
+        'photo': 'http://lorempixel.com/350/200/food/',
+        'horarios': '13 - 22'
+        'precios': '$260.00'
+    },
+]
